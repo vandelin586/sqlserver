1cbcc7e1a77be07f37191b1346623ea5d34a92a8
Vandelinder <rvandelinder@fs.fed.us>
2017-05-17 12:53:45 -0500

    add file download to install

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,8 +19,24 @@ class winsqlserver::install(
   String $installshareddir,
   String $installsharewowdir,
   Integer $updateenabled,
-)
-  sqlserver_instance{ $instance:
+)include ::sqlserver::params
+
+  if ($install) {
+    # Download SQL Server .zip files and un-compress on server.
+    $download = "puppet:///modules/winsqlserver/${sql_version}.ps1"
+    exec { 'sqlserver_dnld':
+      command  => file($download),
+      provider => powershell,
+      timeout  => 7200,
+    }
+
+
+
+
+
+
+
+  #sqlserver_instance{ $instance:
 
       #    source                => $source,
       #    features              => $features,
@@ -49,22 +65,25 @@ class winsqlserver::install(
           #}
           #}
 
-    }
+
       windowsfeature { 'NET-Framework-Core':
       ensure => present,
       }
+    }
 
 
-            'TCPENABLED'          => $enable_tcp,
-            'SQLTEMPDBLOGDIR'     => $dir_log,
-            'SQLUSERDBLOGDIR'     => $dir_log,
-            'SQLBACKUPDIR'        => $dir_backup,
-            'SQLTEMPDBDIR'        => $dir_tmp,
-            'INSTALLSQLDATADIR'   => $dir_data,
-            'INSTANCEDIR'         => $dir_inst,
-            'INSTALLSHAREDDIR'    => $dir_share,
-            'INSTALLSHAREDWOWDIR' => $dir_wow,
-            'UpdateEnabled'       => 0,
+        #    'TCPENABLED'          => $enable_tcp,
+        #   'SQLTEMPDBLOGDIR'     => $dir_log,
+        #    'SQLUSERDBLOGDIR'     => $dir_log,
+        #    'SQLBACKUPDIR'        => $dir_backup,
+        #    'SQLTEMPDBDIR'        => $dir_tmp,
+        #    'INSTALLSQLDATADIR'   => $dir_data,
+        #    'INSTANCEDIR'         => $dir_inst,
+        #    'INSTALLSHAREDDIR'    => $dir_share,
+        #    'INSTALLSHAREDWOWDIR' => $dir_wow,
+        #    'UpdateEnabled'       => 0,
+
+
 
 
 
@@ -79,87 +98,3 @@ class winsqlserver::install(
   #notify {'inst':
   #  message  => $install,
   #}
-
-# Ensure .NET3.5 is installled
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#sqlserver_instance{ 'MSSQLSERVER':
-#  ensure                => 'present',
-#  features              => ['SQL'],
-#  source                => 'c:/temp',
-#  sql_sysadmin_accounts => ['administrator'],
-
-
-
-
-
-
-
-
-
-#class sqlserver:install {
-# Ensure .NET3.5 is installled
-#  windowsfeature { 'NET-Framework-Core':
-#    ensure => present,
-#  } ->
-
-# Install SQL Server
-#  sqlserver_instance{ $instance :
-#    source                => $source,
-#    features              => $features,
-#    security_mode         => $sec_mode,
-#    sql_sysadmin_accounts => $sa_acct,
-#    sql_svc_account       => $svc_acct,
-#    sql_svc_password      => $svc_pwd,
-#       install_switches      => {
-#    'TCPENABLED'          => $enable_tcp,
-#    'SQLTEMPDBLOGDIR'     => $dir_log,
-#    'SQLUSERDBLOGDIR'     => $dir_log,
-#    'SQLBACKUPDIR'        => $dir_backup,
-#    'SQLTEMPDBDIR'        => $dir_tmp,
-#    'INSTALLSQLDATADIR'   => $dir_data,
-#    'INSTANCEDIR'         => $dir_inst,
-#    'INSTALLSHAREDDIR'    => $dir_share,
-#    'INSTALLSHAREDWOWDIR' => $dir_wow,
-#    'UpdateEnabled'       => 0,
-#      },
-#  }
-#  sqlserver_features { 'Generic Features':
-#    source   => $source,
-#    features => ['IS', 'MDS', 'SSMS'],
-#    install_switches   => {
-#    'UpdateEnabled'  => 0,
-    #}
-    #}
-
-
-#}
-
-
-
-
-
-
-
-
-
-
-
-
-
-#sqlserver_instance{ 'MSSQLSERVER':
-    #features                => ['SQL'],
-    #source                  => 'E:/',
-    #sql_sysadmin_accounts   => ['myuser'],
