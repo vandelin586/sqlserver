dce98834d5db5139558380a3e98bb01eec2ab280
Vandelinder <rvandelinder@fs.fed.us>
2017-05-23 14:40:54 -0500

    set SQL module to service ensure to handle SQL services

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,8 +1,29 @@
 #service
-class winsqlserver::service {
+#class winsqlserver::service {
 
+#service { 'MSSQL$TEST':
+#  ensure => running,
+#  enable => true,
+#  hasrestart => true,
+#  hasstatus  => true,
+  # pattern => 'name',
+#}
+#service { 'SQLBrowser':
+#  ensure => running,
+#  enable => true,
+#  hasrestart => true,
+#  hasstatus  => true,
+  # pattern => 'name',
+#}
+#service { 'SQLWriter':
+#  ensure => running,
+#  enable => true,
+#  hasrestart => true,
+#  hasstatus  => true,
+  # pattern => 'name',
+#}
 #  Running  MSSQL$TEST         SQL Server (TEST)
 #  Stopped  SQLAgent$TEST      SQL Server Agent (TEST)
 #  Running  SQLBrowser         SQL Server Browser
 #  Running  SQLWriter          SQL Server VSS Writer
-}
+#}
